2f98459f74a45b9c08fd165ebf5f5d4c8a806c87
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-10-25 20:04:18 -0300

    add model

---
+++ my_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    model = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.model}"
+
+        
