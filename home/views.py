ab91109405d0b652d955d6dad6a6979d510ce61f
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-10-26 21:30:50 -0300

    clase complementaria

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(
+        request=request,
+        context={},
+        template_name="home/index.html",
+    )
+
+
--- my_app/models.py
+++ my_app/models.py
@@ -7,4 +7,9 @@ class Car(models.Model):
     def __str__(self):
         return f"{self.name} - {self.model}"
 
-        
+class House(models.Model):
+    name = models.CharField(max_length=40)
+    model = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.model}"      
